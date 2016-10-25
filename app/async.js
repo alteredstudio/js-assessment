6e25bb6b25343c5877a53702e2b65b6de94e763d
John MacMenamin <hellojohnnymac@Healthys-MacBook-Pro.local>
2016-10-25 11:37:27 -0500

    Checking in js tests.

--- app/async.js
+++ app/async.js
@@ -2,12 +2,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define([ 'jquery' ], function($) {
   return {
-    async : function(value) {
-
+    async : function(Value) {
+      var data = $.Deferred();
+      setTimeout(function() {
+        data.resolve(Value);
+      }, 15);
+      return data.promise();
     },
-
-    manipulateRemoteData : function(url) {
-
+    manipulateRemoteData : function(URL) {
+      var data = $.Deferred();
+      $.ajax(URL).then(function(resp) {
+        var people = $.map(resp.people, function(person) {
+          return person.name;
+        });
+        data.resolve(people.sort());
+      });
+      return data.promise();
     }
   };
 });
--- app/regex.js
+++ app/regex.js
@@ -2,27 +2,29 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
-    containsNumber : function(str) {
+    containsNumber: function(str) {
+        return (/\d/).test(str);
+      },
 
-    },
+      containsRepeatingLetter: function(str) {
+        return (/([A-Za-z])\1/).test(str);
+      },
 
-    containsRepeatingLetter : function(str) {
+      endsWithVowel: function(str) {
+        return (/[aeiou]$/i).test(str);
+      },
 
-    },
+      captureThreeNumbers: function(str) {
+        var matches = (/\d{3}/).exec(str);
+        return matches ? matches[0] : false;
+      },
 
-    endsWithVowel : function(str) {
+      matchesPattern: function(str) {
+        return (/^\d{3}-\d{3}-\d{4}$/).test(str);
+      },
 
-    },
-
-    captureThreeNumbers : function(str) {
-
-    },
-
-    matchesPattern : function(str) {
-
-    },
-    isUSD : function(str) {
-
-    }
+      isUSD: function(str) {
+        return (/^\$\d{1,3}(,\d{3})*(\.\d{2})?$/).test(str);
+      }
   };
 });
